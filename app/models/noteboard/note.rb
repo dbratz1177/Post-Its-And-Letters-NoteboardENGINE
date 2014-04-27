17b3ae22f85d5c49dccb209fa570923d3ff62c20
Cleophus Robinson IV <CleoR41@gmail.com>
2014-04-27 17:08:01 -0400

    Added an association and took note of some validations that need to happen in the note model such as make sure poster_id is a real user and make sure noteboard_id is a real noteboard.

--- app/models/noteboard/note.rb
+++ app/models/noteboard/note.rb
@@ -1,4 +1,7 @@
 module Noteboard
   class Note < ActiveRecord::Base
+    belongs_to :noteboard  
+    #Need a before call back that makes sure a poster_id
+    #is a user. And that the noteboard_id is a real noteboard
   end
 end
